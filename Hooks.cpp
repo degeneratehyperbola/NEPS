c60f0d1a141f117c959c76673b19235b5f5ee2a5
degeneratehyperbola <65950527+degeneratehyperbola@users.noreply.github.com>
2021-03-21 19:28:43 +0200

    Check for nulls

--- Hooks.cpp
+++ Hooks.cpp
@@ -308,7 +308,9 @@ static bool __fastcall svCheatsGetBool(void* _this) noexcept
 	if (uintptr_t(_ReturnAddress()) == memory->cameraThink && config->visuals.thirdPerson.keyMode)
 		return true;
 
-	return hooks->svCheats.getOriginal<bool, 13>()(_this);
+	auto original = hooks->svCheats.getOriginal<bool, 13>();
+	if (original)
+		original(_this);
 }
 
 static void __stdcall paintTraverse(unsigned int panel, bool forceRepaint, bool allowForce) noexcept
