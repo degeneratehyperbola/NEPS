4b873b372cea11b23a6f22cecaf11782b3d05df3
degeneratehyperbola <65950527+degeneratehyperbola@users.noreply.github.com>
2021-06-13 22:02:31 +0300

    Fix compilation error

--- NEPS/lib/Helpers.cpp
+++ NEPS/lib/Helpers.cpp
@@ -614,3 +614,28 @@ bool Helpers::animDataAuthenticity(Entity *animatable) noexcept
 
 	return false;
 }
+
+std::string Helpers::decode(std::string in) noexcept
+{
+	std::string out;
+
+	for (auto &c : in) --c;
+
+	std::vector<int> T(256, -1);
+	for (int i = 0; i < 64; i++) T["ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"[i]] = i;
+
+	int val = 0, valb = -8;
+	for (unsigned char c : in)
+	{
+		if (T[c] == -1) break;
+		val = (val << 6) + T[c];
+		valb += 6;
+		if (valb >= 0)
+		{
+			out.push_back(char((val >> valb) & 0xFF));
+			valb -= 8;
+		}
+	}
+
+	return out;
+}
--- NEPS/lib/Helpers.hpp
+++ NEPS/lib/Helpers.hpp
@@ -152,28 +152,5 @@ namespace Helpers
 		return buffer;
 	}
 
-	constexpr std::string decode(std::string in)
-	{
-		std::string out;
-
-		for (auto &c : in) --c;
-
-		std::vector<int> T(256, -1);
-		for (int i = 0; i < 64; i++) T["ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"[i]] = i;
-
-		int val = 0, valb = -8;
-		for (unsigned char c : in)
-		{
-			if (T[c] == -1) break;
-			val = (val << 6) + T[c];
-			valb += 6;
-			if (valb >= 0)
-			{
-				out.push_back(char((val >> valb) & 0xFF));
-				valb -= 8;
-			}
-		}
-
-		return out;
-	}
+	std::string decode(std::string) noexcept;
 }
