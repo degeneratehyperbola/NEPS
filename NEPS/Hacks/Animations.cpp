1589e9a191f7e63dbc1bcffba9c9deecda1a29ed
degeneratehyperbola <65950527+degeneratehyperbola@users.noreply.github.com>
2021-08-01 17:46:35 +0300

    Try a different resolver

--- NEPS/Hacks/Animations.cpp
+++ NEPS/Hacks/Animations.cpp
@@ -37,11 +37,13 @@ bool Animations::animDesynced(const UserCmd &cmd, bool sendPacket) noexcept
 		spawnTime = localPlayer->spawnTime();
 	}
 
+	const auto layers = localPlayer->animationLayers();
+
 	static std::array<AnimLayer, MAX_ANIM_OVERLAYS> lerpedLayers;
 
 	if (sendPacket)
 	{
-		std::copy(localPlayer->animationLayers(), localPlayer->animationLayers() + localPlayer->getAnimationLayerCount(), lerpedLayers.begin());
+		std::copy(layers, layers + localPlayer->getAnimationLayerCount(), lerpedLayers.begin());
 
 		const auto backupPoseParam = localPlayer->poseParam();
 		const auto backupAbsYaw = localPlayer->getAbsAngle().y;
@@ -52,9 +54,8 @@ bool Animations::animDesynced(const UserCmd &cmd, bool sendPacket) noexcept
 		memory->invalidateBoneCache(localPlayer.get());
 		memory->setAbsAngle(localPlayer.get(), Vector{0.0f, lerpedState->feetYaw, 0.0f});
 
-		std::copy(lerpedLayers.begin(), lerpedLayers.end(), localPlayer->animationLayers());
-		if (const auto layers = localPlayer->animationLayers())
-			layers[12].weight = FLT_EPSILON;
+		std::copy(lerpedLayers.begin(), lerpedLayers.end(), layers);
+		layers[12].weight = FLT_EPSILON;
 
 		matrixUpdated = localPlayer->setupBones(lerpedBones.data(), MAX_STUDIO_BONES, BONE_USED_BY_ANYTHING, memory->globalVars->currenttime);
 
@@ -80,6 +81,7 @@ bool Animations::animSynced(const UserCmd &cmd, bool sendPacket) noexcept
 	if (!memory->input->isCameraInThirdPerson || !config->misc.fixAnimation)
 		return matrixUpdated;
 
+	const auto layers = localPlayer->animationLayers();
 	auto state = localPlayer->getAnimState();
 
 	static auto backupPoseParam = localPlayer->poseParam();
@@ -90,7 +92,7 @@ bool Animations::animSynced(const UserCmd &cmd, bool sendPacket) noexcept
 
 	static std::array<AnimLayer, MAX_ANIM_OVERLAYS> networkedLayers;
 
-	std::copy(localPlayer->animationLayers(), localPlayer->animationLayers() + localPlayer->getAnimationLayerCount(), networkedLayers.begin());
+	std::copy(layers, layers + localPlayer->getAnimationLayerCount(), networkedLayers.begin());
 
 	localPlayer->clientAnimations() = true;
 	memory->updateState(state, NULL, NULL, cmd.viewangles.y, cmd.viewangles.x, NULL);
@@ -107,7 +109,7 @@ bool Animations::animSynced(const UserCmd &cmd, bool sendPacket) noexcept
 	state->duckAmount = std::clamp(state->duckAmount, 0.0f, 1.0f);
 	state->feetYawRate = 0.0f;
 	memory->setAbsAngle(localPlayer.get(), Vector{0.0f, backupAbsYaw, 0.0f});
-	std::copy(networkedLayers.begin(), networkedLayers.end(), localPlayer->animationLayers());
+	std::copy(networkedLayers.begin(), networkedLayers.end(), layers);
 	localPlayer->poseParam() = backupPoseParam;
 	localPlayer->clientAnimations() = true;
 
@@ -136,16 +138,10 @@ void Animations::resolveLBY(Entity *animatable, int misses) noexcept
 	memory->updateState(state, nullptr, animatable->eyeAngles().x, animatable->eyeAngles().y, 0.0f, nullptr);
 	animatable->clientAnimations() = false;
 
-	if (!animatable->animationLayers())
-		return;
-
-	std::array<AnimLayer, MAX_ANIM_OVERLAYS> layers;
-	std::copy(animatable->animationLayers(), animatable->animationLayers() + animatable->getAnimationLayerCount(), layers.begin());
-
-	const auto delta = Helpers::angleDiffDeg(state->feetYaw, animatable->eyeAngles().y);
+	const auto layers = animatable->animationLayers();
 
 	std::array<float, 3U> records;
-	const auto bFeetYaw = state->feetYaw;
+	const auto backupFeetYaw = state->feetYaw;
 
 	state->feetYaw = animatable->eyeAngles().y;
 	animatable->setupBones(nullptr, MAX_STUDIO_BONES, BONE_USED_BY_ANYTHING, memory->globalVars->currenttime);
@@ -159,33 +155,30 @@ void Animations::resolveLBY(Entity *animatable, int misses) noexcept
 	animatable->setupBones(nullptr, MAX_STUDIO_BONES, BONE_USED_BY_ANYTHING, memory->globalVars->currenttime);
 	records[2] = layers[6].playbackRate;
 
-	state->feetYaw = bFeetYaw;
+	state->feetYaw = backupFeetYaw;
 
-	if (!layers[3].weight && !layers[3].cycle || layers[3].sequence == 979)
+	signed char side = 0;
+	if (animatable->velocity().length2D() < 0.1f)
 	{
-		if (delta < 0.0f)
-			state->feetYaw = animatable->eyeAngles().y - animatable->getMaxDesyncAngle();
-		else
-			state->feetYaw = animatable->eyeAngles().y + animatable->getMaxDesyncAngle();
+		const auto delta = Helpers::angleDiffDeg(state->feetYaw, animatable->eyeAngles().y);
+
+		if (layers[3].weight == 0.0f && layers[3].cycle == 0.0f)
+			side = delta <= 0.0f ? 1 : -1;
 	}
-	else if (static_cast<int>(layers[12].weight * 1000) == static_cast<int>(layers[6].weight * 1000) || !static_cast<int>(layers[12].weight * 1000))
+	else if (!static_cast<int>(layers[12].weight * 1000) && static_cast<int>(layers[12].weight * 1000) == static_cast<int>(layers[6].weight * 1000))
 	{
 		const auto a = std::fabsf(layers[6].playbackRate - records[0]);
 		const auto b = std::fabsf(layers[6].playbackRate - records[1]);
 		const auto c = std::fabsf(layers[6].playbackRate - records[2]);
 
-		if (a < c || b <= c || (c * 1000))
+		if (a < c || b <= c || static_cast<int>(c * 1000))
 		{
-			if (a >= c && b > c && !(c * 1000))
-				state->feetYaw = animatable->eyeAngles().y - animatable->getMaxDesyncAngle();
-		} else
-			state->feetYaw = animatable->eyeAngles().y + animatable->getMaxDesyncAngle();
+			if (a >= b && c > b && !static_cast<int>(b * 1000))
+				side = 1;
+		} else side = -1;
 	}
-	else if (misses > 3)
-	{
-		const std::array<float, 3> positions = {animatable->getMaxDesyncAngle(), 0.0f, -animatable->getMaxDesyncAngle()};
-		state->feetYaw = animatable->eyeAngles().y + positions[misses % positions.size()];
-	}
+
+	state->feetYaw = animatable->eyeAngles().y - animatable->getMaxDesyncAngle() * side;
 
 	state->duckAmount = std::clamp(state->duckAmount, 0.0f, 1.0f);
 	state->feetYawRate = 0.0f;
