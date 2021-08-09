bbd85a5a30fbcd54861bfbff954a8fdd9d9470eb
degeneratehyperbola <65950527+degeneratehyperbola@users.noreply.github.com>
2021-08-09 17:33:54 +0300

    Remove old find pattern algorythm

--- NEPS/Memory.h
+++ NEPS/Memory.h
@@ -184,45 +184,6 @@ public:
 
 		return 0;
 	}
-
-	#ifdef OLD_FIND_PAT
-	static std::uintptr_t findPattern(const wchar_t *moduleName, const char *pattern) noexcept
-	{
-		static auto id = 0;
-		++id;
-
-		if (HMODULE moduleHandle = GetModuleHandleW(moduleName))
-		{
-			if (MODULEINFO moduleInfo; GetModuleInformation(GetCurrentProcess(), moduleHandle, &moduleInfo, sizeof(moduleInfo)))
-			{
-				auto start = static_cast<const char *>(moduleInfo.lpBaseOfDll);
-				const auto end = start + moduleInfo.SizeOfImage;
-				auto first = start;
-				auto second = pattern;
-				while (first < end && *second)
-				{
-					if (*first == *second || *second == '?')
-					{
-						++first;
-						++second;
-					} else
-					{
-						first = ++start;
-						second = pattern;
-					}
-				}
-				if (!*second)
-					return reinterpret_cast<std::uintptr_t>(start);
-			}
-		}
-
-		#ifdef _WIN32
-		MessageBoxA(NULL, ("Failed to find pattern #" + std::to_string(id) + '!').c_str(), "NEPS", MB_OK | MB_ICONWARNING);
-		#endif
-
-		return 0;
-	}
-	#endif // OLD_FIND_PAT
 };
 
 inline std::unique_ptr<const Memory> memory;
