fb0ec6031d58d51612ca601b2a2a96e3e2df5561
degeneratehyperbola <65950527+degeneratehyperbola@users.noreply.github.com>
2021-09-02 22:05:18 +0300

    Share "stb" and "Texture" libs

---
+++ shared_lib/Texture/TextureDX9.cpp
@@ -0,0 +1,48 @@
+#include "TextureDX9.h"
+#include <shared_lib/imgui/imgui_impl_dx9.h>
+#include <shared_lib/stb/image.h>
+#include <NEPS/Hooks.h>
+
+#include <memory>
+#include <d3d9.h>
+
+Texture::Texture(const char *path) noexcept
+{
+	if (!path) return;
+
+	int width, height;
+	auto data = stbi_load(path, &width, &height, nullptr, 4);
+	texture = reinterpret_cast<IDirect3DTexture9 *>(ImGui_ImplDX9_CreateTextureRGBA8(width, height, data));
+	stbi_image_free(data);
+}
+
+Texture::Texture(int resource, const char *type) noexcept
+{
+	if (!resource || !type) return;
+
+	auto dllHandle = hooks->getDllHandle();
+	auto resourceHandle = FindResourceA(dllHandle, MAKEINTRESOURCEA(resource), type);
+	if (resourceHandle)
+	{
+		int width, height;
+		int resourceSize = SizeofResource(dllHandle, resourceHandle);
+		auto resourceData = LoadResource(dllHandle, resourceHandle);
+		void *buffer = LockResource(resourceData);
+		auto data = stbi_load_from_memory(reinterpret_cast<unsigned char *>(buffer), resourceSize, &width, &height, nullptr, 4);
+		texture = reinterpret_cast<IDirect3DTexture9 *>(ImGui_ImplDX9_CreateTextureRGBA8(width, height, data));
+		stbi_image_free(data);
+		FreeResource(resourceData);
+	}
+}
+
+Texture::Texture(int width, int height, const unsigned char *data) noexcept
+{
+	if (!data) return;
+
+	texture = reinterpret_cast<IDirect3DTexture9 *>(ImGui_ImplDX9_CreateTextureRGBA8(width, height, data));
+}
+
+Texture::~Texture() noexcept
+{
+	if (texture) texture->Release();
+}
