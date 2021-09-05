496dd34e76e6737ad3dbe802591f82e1d843fa11
degeneratehyperbola <65950527+degeneratehyperbola@users.noreply.github.com>
2021-09-05 18:49:09 +0300

    Move NLoader lib to `/lib`

--- NLoader/NLoader.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <string>
-
-namespace NLoader
-{
-	int __stdcall loadLibrary(const void *dllBuffer, unsigned long pid, unsigned long *dllMainReturn) noexcept;
-	unsigned long findPID(const std::wstring &processName) noexcept;
-}
