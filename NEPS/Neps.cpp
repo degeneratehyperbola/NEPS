f7dce990fbe61640a1de3b7f316c8f3a9ab610ae
degeneratehyperbola <65950527+degeneratehyperbola@users.noreply.github.com>
2021-09-05 18:27:11 +0300

    Remove signature verification

--- NEPS/Neps.cpp
+++ NEPS/Neps.cpp
@@ -1,10 +1,6 @@
 #include <clocale>
 #include "Hooks.h"
 
-#define SIGNATURE 0x7EFA32B4
-#define SIGNATURE_MASK 0x0000000F
-//#define SIGNATURE_ENABLE // Uncomment this to enable signature verification
-
 extern "C" BOOL WINAPI _CRT_INIT(HMODULE moduleHandle, DWORD reason, LPVOID reserved);
 
 static DWORD WINAPI unload(HMODULE moduleHandle) noexcept
@@ -16,23 +12,14 @@ static DWORD WINAPI unload(HMODULE moduleHandle) noexcept
 
 BOOL APIENTRY DllEntryPoint(HMODULE moduleHandle, DWORD reason, LPVOID reserved)
 {
-	if (!_CRT_INIT(moduleHandle, reason & SIGNATURE_MASK, reserved))
+	if (!_CRT_INIT(moduleHandle, reason, reserved))
 		return FALSE;
 
-	if ((reason & SIGNATURE_MASK) == DLL_PROCESS_ATTACH)
+	if (reason == DLL_PROCESS_ATTACH)
 	{
-		#if defined SIGNATURE_ENABLE && !defined NEPS_DEBUG
-		if ((reason & ~SIGNATURE_MASK) == (SIGNATURE & ~SIGNATURE_MASK))
-		#endif // NEPS_DEBUG
-		{
-			std::setlocale(LC_CTYPE, ".utf8");
-			hooks = std::make_unique<Hooks>(moduleHandle);
-			return TRUE;
-		}
-
-		MessageBoxA(nullptr, "Oh no! Somehow DLL got injected outside of loader. I'm sorry about that.", "NEPS.PP", MB_OK | MB_ICONERROR);
-		if (HANDLE thread = CreateThread(nullptr, 0, LPTHREAD_START_ROUTINE(unload), moduleHandle, 0, nullptr))
-			CloseHandle(thread);
+		std::setlocale(LC_CTYPE, ".utf8");
+		hooks = std::make_unique<Hooks>(moduleHandle);
+		return TRUE;
 	}
 
     return FALSE;
