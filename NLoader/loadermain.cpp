74fc9c87b870affd1c7861c55df392ee5d247141
degeneratehyperbola <65950527+degeneratehyperbola@users.noreply.github.com>
2021-09-05 18:51:38 +0300

    Use LoadStream library for remote dll injection

--- NLoader/loadermain.cpp
+++ NLoader/loadermain.cpp
@@ -1,34 +1,43 @@
-#include "NLoader.h"
+#include "lib/NLoader.hpp"
 #include "resource.h"
 
 #include <Windows.h>
+#include <ctime>
+#include <filesystem>
 
-BOOL WINAPI WinMain(HINSTANCE instance, HINSTANCE previousInstance, char *cmdLine, int showCmd)
+BOOL RemoteLoadStreamLibraryW(ULONG, PBYTE, LPCWSTR), RemoteLoadStreamLibraryA(ULONG, PBYTE, LPCSTR);
+
+int WINAPI WinMain(HINSTANCE instance, HINSTANCE previousInstance, LPSTR cmdLine, int showCmd)
 {
-	const HMODULE moduleHandle = GetModuleHandleA(0);
-	const HRSRC resource = FindResourceA(moduleHandle, MAKEINTRESOURCEA(IDR_BIN1), "BIN");
-	const HGLOBAL resourceData = LoadResource(moduleHandle, resource);
-	const void *dllBuffer = LockResource(resourceData);
+	std::size_t size;
+	const auto resource = NLoader::loadFromResource(&size);
+	const void *dllData = LockResource(resource);
 
-	const auto csgoPid = NLoader::findPID(L"csgo.exe");
+	const auto csgoPid = NLoader::findPid(L"csgo.exe");
 	if (!csgoPid)
 		MessageBoxA(0, "Falied to load NEPS.\nYou need to run CS:GO before running the loader.", "NEPS", MB_OK | MB_ICONERROR);
 	else
 	{
-		DWORD returnValue = 0;
+		wchar_t tempPath[MAX_PATH];
+		std::size_t charCount = GetTempPath(MAX_PATH, tempPath);
 
-		if (!NLoader::loadLibrary(dllBuffer, csgoPid, &returnValue))
-			MessageBoxA(0, "Falied to load NEPS.\nInvalid PID.", "NEPS", MB_OK | MB_ICONERROR);
-		else
+		for (const auto &entry : std::filesystem::directory_iterator(tempPath))
 		{
-			if (!returnValue)
-				MessageBoxA(0, "Falied to load NEPS.\nTry running the loader with administrator privileges.", "NEPS", MB_OK | MB_ICONERROR);
-			else
-				MessageBoxA(0, "Success! NEPS is now loaded.", "NEPS", MB_OK | MB_ICONINFORMATION);
+			if (entry.is_regular_file() && entry.path().filename().wstring().ends_with(L".neps.tempalloc"))
+				std::filesystem::remove(entry.path());
 		}
+
+		wchar_t tempFile[MAX_PATH];
+		std::srand(static_cast<int>(std::time(0)));
+		std::swprintf(tempFile, MAX_PATH, L"%ls\\%i.neps.tempalloc", tempPath, std::rand());
+
+		if (!RemoteLoadStreamLibraryW(csgoPid, (PBYTE)dllData, tempFile))
+			MessageBoxA(0, "Falied to load NEPS.\nUnknown error occured while loading library.", "NEPS", MB_OK | MB_ICONERROR);
+		else
+			MessageBoxA(0, "Success! NEPS is now loaded.", "NEPS", MB_OK | MB_ICONINFORMATION);
 	}
 
-	FreeResource(resourceData);
+	FreeResource(resource);
 
 	return TRUE;
 }
