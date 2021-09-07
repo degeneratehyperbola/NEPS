d6b1ad505d9135975bee851baf0568cb3a1c62c5
degeneratehyperbola <65950527+degeneratehyperbola@users.noreply.github.com>
2021-09-07 19:36:30 +0300

    Use native dll inject method instead of manual map

--- NLoader/lib/Helpers.hpp
+++ NLoader/lib/Helpers.hpp
@@ -9,4 +9,6 @@ namespace Helpers
 	void *loadFromResource(std::size_t *size = nullptr) noexcept;
 	unsigned long findPid(const std::wstring &processName) noexcept;
 	bool fileInUse(const std::filesystem::path &) noexcept;
+	bool loadLibreryRemote(void *process, const std::filesystem::path &, std::uintptr_t *moduleHandle = nullptr) noexcept;
+	bool bypassCsgoInject(void *csgoProcess) noexcept;
 }
--- NLoader/loadermain.cpp
+++ NLoader/loadermain.cpp
@@ -4,8 +4,7 @@
 #include <Windows.h>
 #include <ctime>
 #include <filesystem>
-
-BOOL RemoteLoadStreamLibraryW(ULONG, PBYTE, LPCWSTR), RemoteLoadStreamLibraryA(ULONG, PBYTE, LPCSTR);
+#include <fstream>
 
 int WINAPI WinMain(HINSTANCE instance, HINSTANCE previousInstance, LPSTR cmdLine, int showCmd)
 {
@@ -14,7 +13,7 @@ int WINAPI WinMain(HINSTANCE instance, HINSTANCE previousInstance, LPSTR cmdLine
 
 	for (const auto &entry : std::filesystem::directory_iterator(tempPath))
 	{
-		if (entry.is_regular_file() && !Helpers::fileInUse(entry.path()) && entry.path().filename().wstring().ends_with(L".neps.tempalloc"))
+		if (entry.is_regular_file() && !Helpers::fileInUse(entry.path()) && entry.path().filename().wstring().ends_with(L"-neps-tmp.dll"))
 			std::filesystem::remove(entry.path());
 	}
 
@@ -23,20 +22,47 @@ int WINAPI WinMain(HINSTANCE instance, HINSTANCE previousInstance, LPSTR cmdLine
 		MessageBoxA(0, "Falied to load NEPS.\nYou need to run CS:GO before running the loader.", "NEPS", MB_OK | MB_ICONERROR);
 	else
 	{
-		wchar_t tempFile[MAX_PATH];
-		std::srand(static_cast<int>(std::time(0)));
-		std::swprintf(tempFile, MAX_PATH, L"%ls\\%i.neps.tempalloc", tempPath, std::rand());
-
-		std::size_t size;
-		const auto resource = Helpers::loadFromResource(&size);
-		const void *dllData = LockResource(resource);
-
-		if (!RemoteLoadStreamLibraryW(csgoPid, (PBYTE)dllData, tempFile))
-			MessageBoxA(0, "Falied to load NEPS.\nUnknown error occured while loading library.", "NEPS", MB_OK | MB_ICONERROR);
+		auto csgoProcess = OpenProcess(PROCESS_ALL_ACCESS, false, csgoPid);
+		if (!csgoProcess)
+			MessageBoxA(0, "Falied to load NEPS.\nYou need to run CS:GO before running the loader.", "NEPS", MB_OK | MB_ICONERROR);
 		else
-			MessageBoxA(0, "Success! NEPS is now loaded.", "NEPS", MB_OK | MB_ICONINFORMATION);
+		{
+			if (!Helpers::bypassCsgoInject(csgoProcess))
+				MessageBoxA(0, "Falied to load NEPS.\nFailed to bypass CS:GO library inject protection.", "NEPS", MB_OK | MB_ICONERROR);
+			else
+			{
+				wchar_t tempFilePath[MAX_PATH];
+				std::srand(static_cast<int>(std::time(nullptr)));
+				std::swprintf(tempFilePath, MAX_PATH, L"%ls\\%i-neps-tmp.dll", tempPath, std::rand());
+
+				std::ofstream tempFile{tempFilePath, std::ios::binary | std::ios::out};
+				if (tempFile.is_open())
+				{
+					std::size_t size;
+					const auto resource = Helpers::loadFromResource(&size);
+					const void *dllData = LockResource(resource);
+
+					tempFile.write(reinterpret_cast<const char *>(dllData), size);
+					tempFile.close();
+
+					std::uintptr_t dllModuleHandle = 0;
+					if (!Helpers::loadLibreryRemote(csgoProcess, tempFilePath, &dllModuleHandle))
+						MessageBoxA(0, "Falied to load NEPS.\nUnknown error has occured while loading library.", "NEPS", MB_OK | MB_ICONERROR);
+					else
+					{
+						if (!dllModuleHandle)
+							MessageBoxA(0, "Falied to load NEPS.\nDllMain returned false or load library failed.", "NEPS", MB_OK | MB_ICONERROR);
+						else
+							MessageBoxA(0, "Success! NEPS is now loaded.", "NEPS", MB_OK | MB_ICONINFORMATION);
+					}
 		
-		FreeResource(resource);
+					FreeResource(resource);
+				} else
+					MessageBoxA(0, "Falied to load NEPS.\nFailed to create temporary file.", "NEPS", MB_OK | MB_ICONERROR);
+			}
+
+			CloseHandle(csgoProcess);
+		}
 	}
 
 	return TRUE;
