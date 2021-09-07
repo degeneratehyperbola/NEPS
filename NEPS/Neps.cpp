b38d31f8409cb62cd251ef779da2cdb3d9034f32
degeneratehyperbola <65950527+degeneratehyperbola@users.noreply.github.com>
2021-09-07 19:31:11 +0300

    Remove unused function

--- NEPS/Neps.cpp
+++ NEPS/Neps.cpp
@@ -3,13 +3,6 @@
 
 extern "C" BOOL WINAPI _CRT_INIT(HMODULE moduleHandle, DWORD reason, LPVOID reserved);
 
-static DWORD WINAPI unload(HMODULE moduleHandle) noexcept
-{
-	Sleep(100);
-	_CRT_INIT(moduleHandle, DLL_PROCESS_DETACH, nullptr);
-	FreeLibraryAndExitThread(moduleHandle, 0);
-}
-
 BOOL APIENTRY DllEntryPoint(HMODULE moduleHandle, DWORD reason, LPVOID reserved)
 {
 	if (!_CRT_INIT(moduleHandle, reason, reserved))
